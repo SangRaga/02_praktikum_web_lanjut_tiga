2cea11a290f655b992021878e492c61ea5eafaac
SangRaga <117066100+SangRaga@users.noreply.github.com>
2023-02-26 20:42:27 +0700

    membuat router dan controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,50 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "ini halaman utama";
 });
+Route::prefix('kategori')->group(function () {
+    Route::get('/produk1', function () {
+        return "ini produk1";
+    }
+    );
+    Route::get('/produk2', function () {
+        return "ini produk2";
+    }
+    );
+    Route::get('/produk3', function () {
+        return "ini produk3";
+    }
+    );
+    Route::get('/produk4', function () {
+        return "ini produk4";
+    }
+    );
+});
+Route::prefix('berita')->group(function () {
+    Route::get('/', function () {
+        return "ini halaman berita";
+    }
+    );
+    Route::get('/{id}',[BeritaController::class, ('berita')]
+    );
+});
+Route::prefix('program')->group(function () {
+    Route::get('/karir', function () {
+        return "ini program karir";
+    }
+    );
+    Route::get('/magang', function () {
+        return "ini program magang";
+    }
+    );
+    Route::get('/industri', function () {
+        return "ini program industri";
+    }
+    );
+    
+});
+Route::get('about-us', function (){
+    return "ini about-us";
+});
+Route::resource('contact', ContactController::class);
